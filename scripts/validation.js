74ffd706a821c9dfa5af9499f969d61795c307fc
Erik <eazybeven@gmail.com>
2026-05-13 14:44:40 -0400

    My updated project

--- scripts/validation.js
+++ scripts/validation.js
@@ -42,7 +42,7 @@ const toggleButtonState = (inputList, buttonEl, config) => {
   }
 };
 
-const disableButton = (buttonEl) => {
+const disableButton = (buttonEl, config) => {
   buttonEl.disabled = true;
   buttonEl.classList.add(config.inactiveButtonClass);
 };
